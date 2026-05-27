ec9a75e882cee0b6214b74db497b289e49b47e02
yashodhangurav <guravyashodhan@gmail.com>
2026-05-27 20:51:11 +0530

    practice dijkstras

--- dijkstras.c
+++ dijkstras.c
@@ -1,15 +1,83 @@
+// #include<stdio.h>
+// #include<limits.h>
+
+// void dijkstras(int g[10][10], int n, int src){
+//     int visited[10], dist[10];
+
+//     // initializing
+//     for(int i = 0; i<n; i++){
+//         visited[i] = 0;
+//         dist[i] = INT_MAX;
+//     }
+    
+//     dist[src] = 0;
+
+//     for(int count = 0; count<n-1; count++){
+//         int min = INT_MAX, u;
+
+//         for(int i = 0; i<n; i++){
+//             if(!visited[i] && dist[i]<min){
+//                 min = dist[i];
+//                 u = i;
+//             }
+//         }
+//         visited[u] = 1;
+
+//         for(int v = 0; v<n; v++){
+//             if(!visited[v] && g[u][v] && dist[u] + g[u][v] < dist[v]){
+//                 dist[v] = dist[u] + g[u][v];
+//             }
+//         }
+//     }
+
+
+
+//     // print result
+
+//     for(int i =0; i<n; i++){
+//         printf("%d -> %d = %d\n", src, i, dist[i]);
+//     }
+
+
+// }
+
+
+// int main(){
+//     int g[10][10], n,src;
+
+//     printf("Enter the number of the vertices: ");
+//     scanf("%d", &n);
+
+//     printf("Print the adjucency matrix: \n");
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             scanf("%d", &g[i][j]);
+//         }
+//     }
+
+//     printf("Enter the source vertex: ");
+//     scanf("%d", &src);
+//     dijkstras(g, n, src);
+
+
+//     return 0;
+// }
+
+
+
+
+// ----------------------------------------practice---------------------------------------
+
 #include<stdio.h>
 #include<limits.h>
 
 void dijkstras(int g[10][10], int n, int src){
-    int visited[10];
-    int dist[10];
+    int visited[10], dist[10];
 
     for(int i = 0; i<n; i++){
         visited[i] = 0;
         dist[i] = INT_MAX;
     }
-    
     dist[src] = 0;
 
     for(int count = 0; count<n-1; count++){
@@ -21,29 +89,25 @@ void dijkstras(int g[10][10], int n, int src){
                 u = i;
             }
         }
+
         visited[u] = 1;
 
         for(int v = 0; v<n; v++){
-            if(!visited[v] && g[u][v] && dist[u] + g[u][v] < dist[v]){
-                dist[v] = dist[u] + g[u][v];
+            if(!visited[v] && g[u][v] && dist[u]+g[u][v] < dist[v]){
+                dist[v] = dist[u]+g[u][v];
             }
         }
     }
 
 
-
     // print result
-
-    for(int i =0; i<n; i++){
+    for(int i = 0; i<n; i++){
         printf("%d -> %d = %d\n", src, i, dist[i]);
     }
-
-
 }
 
-
 int main(){
-    int g[10][10], n,src;
+    int g[10][10], n, src;
 
     printf("Enter the number of the vertices: ");
     scanf("%d", &n);
@@ -54,11 +118,7 @@ int main(){
             scanf("%d", &g[i][j]);
         }
     }
-
     printf("Enter the source vertex: ");
     scanf("%d", &src);
     dijkstras(g, n, src);
-
-
-    return 0;
 }
