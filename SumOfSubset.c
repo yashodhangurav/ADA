2b503b7b202171a7cdd51a50522431c21f59e122
yashodhangurav <guravyashodhan@gmail.com>
2026-05-28 13:25:08 +0530

    practice

--- SumOfSubset.c
+++ SumOfSubset.c
@@ -1,4 +1,60 @@
-// find subset with given sum using recursion
+// // find subset with given sum using recursion
+
+// #include<stdio.h>
+
+// int w[10], x[10], d, count = 0;
+
+// void subset(int sum, int k, int n){
+//     if(sum == d){
+//         printf("Subset %d: ", ++count);
+
+//         for(int i = 0; i<n; i++){
+//             if(x[i] == 1){
+//                 printf("%d ", w[i]);
+//             }
+//         }
+//         printf("\n");
+//         return;
+//     }
+
+//     for(int i = k; i<n; i++){
+//         if(sum + w[i] <= d){
+//             x[i] = 1;                     // FIXED
+//             subset(sum + w[i], i + 1, n);
+//             x[i] = 0;
+//         }
+//     }
+// }
+
+// int main(){
+//     int n;
+
+//     printf("Enter number of elements: ");
+//     scanf("%d", &n);
+
+//     printf("Enter elements: ");
+//     for(int i = 0; i<n; i++){
+//         scanf("%d", &w[i]);
+//     }
+
+//     printf("Enter required sum: ");
+//     scanf("%d", &d);
+
+//     subset(0, 0, n);
+
+//     if(count == 0){
+//         printf("No subset found");
+//     }
+
+//     return 0;
+// }
+
+
+
+
+// -------------------------------------------PRACTICE---------------------------
+
+
 
 #include<stdio.h>
 
@@ -6,45 +62,46 @@ int w[10], x[10], d, count = 0;
 
 void subset(int sum, int k, int n){
     if(sum == d){
-        printf("Subset %d: ", ++count);
-
+        printf("subset %d: ", ++count);
+        
         for(int i = 0; i<n; i++){
-            if(x[i] == 1){
-                printf("%d ", w[i]);
+            if(x[i]==1){
+                printf("%d", w[i]);
             }
         }
         printf("\n");
-        return;
     }
-
+    
     for(int i = k; i<n; i++){
-        if(sum + w[i] <= d){
-            x[i] = 1;                     // FIXED
-            subset(sum + w[i], i + 1, n);
-            x[i] = 0;
-        }
+        if(sum+w[i]<=d){
+            x[i] = 1;
+            subset(sum+w[i], i+1, n);
+            x[i] = 0
+;        }
     }
+    
+    
 }
+
 
 int main(){
     int n;
-
-    printf("Enter number of elements: ");
+    printf("Enter the number of the elements:");
     scanf("%d", &n);
-
-    printf("Enter elements: ");
+    
+    printf("Enter the elememts: ");
     for(int i = 0; i<n; i++){
         scanf("%d", &w[i]);
     }
-
-    printf("Enter required sum: ");
+    printf("Enter the required sum: ");
     scanf("%d", &d);
-
-    subset(0, 0, n);
-
+    
+    subset(0,0,n);
     if(count == 0){
-        printf("No subset found");
+        printf("No cubset");
+        
     }
 
     return 0;
+    
 }
