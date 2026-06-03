ebeb27a059a9d0dff0e900598d56b67a522d3bad
yashodhangurav <guravyashodhan@gmail.com>
2026-06-03 19:42:55 +0530

    warshalls practice

--- warshalls.c
+++ warshalls.c
@@ -1,28 +1,81 @@
 // find the transition closure using warshall algo
 // find the transition closure using warshall algo
 
-#include<stdio.h>
+// #include<stdio.h>
 
+
+// void warshalls(int a[10][10], int n){
+//     int t[10][10];
+    
+//     for(int  i = 0; i<n; i++){
+//         for(int j= 0; j<n; j++){
+//             t[i][j] = a[i][j];
+//         }
+//     }
+    
+//     for(int k = 0; k<n; k++){
+//         for(int i= 0; i<n; i++){
+//             for(int j = 0; j<n; j++){
+//                 t[i][j] = t[i][j] || (t[i][k] && t[k][j]);
+//             }
+//         }
+//     }
+    
+//     printf("\n Transition closure is : ");
+//     for(int i=0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             printf("%d\t", t[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// int main(){
+//     int a[10][10], n;
+    
+//     printf("Enter the number of the vertises: ");
+//     scanf("%d", &n);
+    
+//     printf("Enter the adjucency matrix: ");
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+//     warshalls(a,n);
+    
+//     return 0;
+// }
+
+
+
+
+// ----------------------------------PRACTICE-----------------------------------
+
+
+#include<stdio.h>
 
 void warshalls(int a[10][10], int n){
     int t[10][10];
-    
-    for(int  i = 0; i<n; i++){
-        for(int j= 0; j<n; j++){
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
             t[i][j] = a[i][j];
         }
     }
-    
+
+
     for(int k = 0; k<n; k++){
-        for(int i= 0; i<n; i++){
+        for(int i = 0; i<n; i++){
             for(int j = 0; j<n; j++){
                 t[i][j] = t[i][j] || (t[i][k] && t[k][j]);
             }
         }
     }
-    
-    printf("\n Transition closure is : ");
-    for(int i=0; i<n; i++){
+
+
+    // print
+    for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             printf("%d\t", t[i][j]);
         }
@@ -32,17 +85,18 @@ void warshalls(int a[10][10], int n){
 
 int main(){
     int a[10][10], n;
-    
-    printf("Enter the number of the vertises: ");
+
+    printf("Enter the number of the vertices: ");
     scanf("%d", &n);
-    
-    printf("Enter the adjucency matrix: ");
+
+    printf("Enter the adjacency matrix: \n");
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             scanf("%d", &a[i][j]);
         }
     }
+
     warshalls(a,n);
-    
+
     return 0;
 }
