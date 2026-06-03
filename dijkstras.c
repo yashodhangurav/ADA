c134fec2620230ca70ca6e10a5ff431d1a0c1578
yashodhangurav <guravyashodhan@gmail.com>
2026-06-03 21:14:08 +0530

    practice

--- dijkstras.c
+++ dijkstras.c
@@ -68,8 +68,138 @@
 
 // ----------------------------------------practice---------------------------------------
 
+// #include<stdio.h>
+// #include<limits.h>
+
+// void dijkstras(int g[10][10], int n, int src){
+//     int visited[10], dist[10];
+
+//     for(int i = 0; i<n; i++){
+//         visited[i] = 0;
+//         dist[i] = INT_MAX;
+//     }
+//     dist[src] = 0;
+
+//     for(int count = 0; count<n-1; count++){
+//         int min = INT_MAX, u;
+
+//         for(int i = 0; i<n; i++){
+//             if(!visited[i] && dist[i]<min){
+//                 min = dist[i];
+//                 u = i;
+//             }
+//         }
+
+//         visited[u] = 1;
+
+//         for(int v = 0; v<n; v++){
+//             if(!visited[v] && g[u][v] && dist[u]+g[u][v] < dist[v]){
+//                 dist[v] = dist[u]+g[u][v];
+//             }
+//         }
+//     }
+
+
+//     // print result
+//     for(int i = 0; i<n; i++){
+//         printf("%d -> %d = %d\n", src, i, dist[i]);
+//     }
+// }
+
+// int main(){
+//     int g[10][10], n, src;
+
+//     printf("Enter the number of the vertices: ");
+//     scanf("%d", &n);
+
+//     printf("Print the adjucency matrix: \n");
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             scanf("%d", &g[i][j]);
+//         }
+//     }
+//     printf("Enter the source vertex: ");
+//     scanf("%d", &src);
+//     dijkstras(g, n, src);
+// }
+
+
+
+// -----------------------------------------------practice 2 ------------------------------
+
+
+// #include<stdio.h>
+// #include<limits.h>
+
+
+// void dijkstras(int g[10][10], int n, int src){
+    
+//     int visited[10], dist[10];
+
+//     for(int i = 0; i<n; i++){
+//         visited[i] = 0;
+//         dist[i] = INT_MAX;
+//     }
+//     dist[src] = 0;
+
+//     for(int count = 0; count<n-1; count++){
+//         int min = INT_MAX, u;
+
+//         for(int i = 0; i<n; i++){
+//             if(!visited[i] && dist[i]< min){
+//                 min = dist[i];
+//                 u = i;
+//             }
+//         }
+//         visited[u] = 1;
+
+//         for(int v = 0; v<n; v++){
+//             if(!visited[v] && g[u][v] && dist[u]+g[u][v] < dist[v]){
+//                 dist[v] = dist[u]+g[u][v];
+//             }
+//         }
+        
+//     }
+
+//     // print
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             printf("%d -> %d = %d", src, i, dist[i]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+
+// int main(){
+//     int g[10][10], n , src;
+
+//     printf("Enter the number of vertices: ");
+//     scanf("%d", &n);
+
+//     printf("Enter the adjacency matrix: \n");
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             scanf("%d", &g[i][j]);
+//         }
+//     }
+
+//     printf("enter the src :");
+//     scanf("%d", &src);
+
+//     dijkstras(g,n,src);
+
+//     return 0;
+// }
+
+
+
+
+// ---------------------------------------practice 4----------------------------------
+
 #include<stdio.h>
 #include<limits.h>
+
 
 void dijkstras(int g[10][10], int n, int src){
     int visited[10], dist[10];
@@ -78,47 +208,55 @@ void dijkstras(int g[10][10], int n, int src){
         visited[i] = 0;
         dist[i] = INT_MAX;
     }
+
     dist[src] = 0;
 
     for(int count = 0; count<n-1; count++){
         int min = INT_MAX, u;
-
         for(int i = 0; i<n; i++){
             if(!visited[i] && dist[i]<min){
                 min = dist[i];
                 u = i;
             }
         }
-
         visited[u] = 1;
 
         for(int v = 0; v<n; v++){
-            if(!visited[v] && g[u][v] && dist[u]+g[u][v] < dist[v]){
+            if(!visited[v] && g[u][v] && dist[u]+g[u][v]<dist[v]){
                 dist[v] = dist[u]+g[u][v];
             }
         }
     }
 
+    // print
 
-    // print result
     for(int i = 0; i<n; i++){
-        printf("%d -> %d = %d\n", src, i, dist[i]);
+        printf("%d -> %d = %d", src, i, dist[i]);
+        printf("\n");
     }
+    
 }
+
 
 int main(){
     int g[10][10], n, src;
 
+
     printf("Enter the number of the vertices: ");
     scanf("%d", &n);
 
-    printf("Print the adjucency matrix: \n");
+    printf("Enter the adacecy matrix: ");
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             scanf("%d", &g[i][j]);
         }
     }
-    printf("Enter the source vertex: ");
+
+    printf("Enter the source: ");
     scanf("%d", &src);
-    dijkstras(g, n, src);
+
+    dijkstras(g,n,src);
+
+
+    return 0;
 }
--- knapsackZeroOne.c
+++ knapsackZeroOne.c
@@ -1,44 +1,94 @@
 // 0/1 knapsack problem using dynamic programming
 
+// #include<stdio.h>
+
+
+// int max(int a, int b){
+//     return (a>b)? a: b;
+// }
+
+// int knapsack(int n, int w[10], int p[10], int m){
+//    if(n == 0 || m == 0){
+//     return 0;
+//    } 
+
+//    if(w[n]>m){
+//     return knapsack(n-1,w,p,m);
+//    }else{
+//     return max(knapsack(n-1,w,p,m), knapsack(n-1,w,p,m-w[n])+p[n]);
+//    }
+// }
+
+// int main(){
+//     int n,m;
+//     int w[10],p[10];
+
+//     printf("Enter the number of the items: ");
+//     scanf("%d", &n);
+
+//     printf("Enter the weights of the items: ");
+//     for(int i= 1; i<=n; i++){
+//         scanf("%d", &w[i]);
+//     }
+//     printf("Enter the profits of the items: ");
+//     for(int i= 1; i<=n; i++){
+//         scanf("%d", &p[i]);
+//     }
+//     printf("Enter the capacity of the knapsack: ");
+//     scanf("%d", &m);
+
+//     int result = knapsack(n,w,p,m);
+//     printf("The maximum profit is: %d", result);
+
+//     return 0;
+// }
+
+
+
+
+
+// ---------------------------------------------------------PRACTICE-----------------------------
+
+
+
 #include<stdio.h>
 
-
-int max(int a, int b){
-    return (a>b)? a: b;
+int max(int a, int b ){
+    return (a>b)? a : b;
 }
 
 int knapsack(int n, int w[10], int p[10], int m){
-   if(n == 0 || m == 0){
-    return 0;
-   } 
+    if(n == 0 || m == 0){
+        return 0;
+    }
 
-   if(w[n]>m){
-    return knapsack(n-1,w,p,m);
-   }else{
-    return max(knapsack(n-1,w,p,m), knapsack(n-1,w,p,m-w[n])+p[n]);
-   }
+    if(w[n-1]>m){
+        return knapsack(n-1,w,p,m);
+    }else{
+        return max(knapsack(n-1,w,p,m), knapsack(n-1,w,p,m-w[n-1])+p[n-1]);
+    }
 }
 
 int main(){
-    int n,m, result;
-    int w[10],p[10];
+    int n, w[10], p[10], m;
 
-    printf("Enter the number of the items: ");
+    printf("Enter the total number of the items: ");
     scanf("%d", &n);
 
-    printf("Enter the weights of the items: ");
-    for(int i= 1; i<=n; i++){
+    printf("Enter the weight of the items: ");
+    for(int i = 0; i<n; i++){
         scanf("%d", &w[i]);
     }
-    printf("Enter the profits of the items: ");
-    for(int i= 1; i<=n; i++){
+    printf("Profit of the items: ");
+    for(int i = 0; i<n; i++){
         scanf("%d", &p[i]);
     }
-    printf("Enter the capacity of the knapsack: ");
+
+    printf("Enter the cpacity of the knpasack: ");
     scanf("%d", &m);
 
-    result = knapsack(n,w,p,m);
-    printf("The maximum profit is: %d", result);
+    int res = knapsack(n,w,p,m);
 
+    printf("Result is : %d\t", res);
     return 0;
 }
