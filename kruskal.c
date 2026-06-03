bf653e801ad7bfc2bc7f9d77e909a9083bbab74f
yashodhangurav <guravyashodhan@gmail.com>
2026-06-03 18:58:20 +0530

    practice kruskal

--- kruskal.c
+++ kruskal.c
@@ -1,58 +1,122 @@
-#include<stdio.h>
+// #include<stdio.h>
 
+
+// int nc = 1, min_cost = 0;
+
+// int main(){
+//     int n, min;
+//     int a, b, u, v;
+//     int cost[20][20], parent[20];
+
+//     printf("Enter the number of the vertices: ");
+//     scanf("%d", &n);
+
+//     printf("Enter the cost adjacency matrix: \n");
+//     for(int i = 1; i<=n; i++){
+//         for(int j = 1; j<=n; j++){
+//             scanf("%d", &cost[i][j]);
+
+//             if(cost[i][j] == 0){
+//                 cost[i][j] = 999;
+//             }
+//         }
+//     }
+
+//     for(int i = 1; i<=n; i++){
+//         parent[i] = 0;
+//     }
+
+//     printf("Edges in the mini cost spanning tree: \n");
+//    while(nc<n){
+//     min = 999;
+//     for(int i = 1; i<=n; i++){
+//         for(int j = 1; j<=n; j++){
+//             if(cost[i][j]<min){
+//                 min = cost[i][j];
+//                 a = u = i;
+//                 b = v = j;
+//             }
+//         }
+//     }
+
+//     while(parent[u]){
+//         u = parent[u];
+//     }
+//     while(parent[v]){
+//         v = parent[v];
+//     }
+
+//     if(u!=v){
+//         printf("%d edge (%d->%d) = %d\n", nc++, a, b, min);
+//         min_cost+=min;
+//         parent[v] = u;
+//     }
+//     cost[a][b] = cost[b][a] = 999;
+//    }
+//    printf("\n minimum cost = %d\n", min_cost);
+//     return 0;
+// }
+
+
+// -------------------------------------PRACTICE-----------------------------------
+
+
+#include<stdio.h>
 
 int nc = 1, min_cost = 0;
 
 int main(){
-    int n, min;
-    int a, b, u, v;
+    int n,min;
+    int a,b,u,v;
     int cost[20][20], parent[20];
 
-    printf("Enter the number of the vertices: ");
+    printf("Enter the number of thevertices: ");
     scanf("%d", &n);
 
-    printf("Enter the cost adjacency matrix: \n");
+    printf("Enter the cost adjucemcy matric: \n");
     for(int i = 1; i<=n; i++){
         for(int j = 1; j<=n; j++){
             scanf("%d", &cost[i][j]);
+
 
             if(cost[i][j] == 0){
                 cost[i][j] = 999;
             }
         }
     }
-
     for(int i = 1; i<=n; i++){
         parent[i] = 0;
     }
 
-    printf("Edges in the mini cost spanning tree: \n");
-   while(nc<n){
-    min = 999;
-    for(int i = 1; i<=n; i++){
-        for(int j = 1; j<=n; j++){
-            if(cost[i][j]<min){
-                min = cost[i][j];
-                a = u = i;
-                b = v = j;
+    printf("Edges in the mini spanning tree: \n");
+
+    while(nc<n){
+        min = 999;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                if(cost[i][j]<min){
+                    min = cost[i][j];
+                    a = u = i;
+                    b = v = j;
+                }
             }
         }
-    }
 
-    while(parent[u]){
-        u = parent[u];
-    }
-    while(parent[v]){
-        v = parent[v];
-    }
+        while(parent[u]){
+            u = parent[u];
+        }
+        while(parent[v]){
+            v = parent[v];
+        }
 
-    if(u!=v){
-        printf("%d edge (%d->%d) = %d\n", nc++, a, b, min);
-        min_cost+=min;
-        parent[v] = u;
+        if(u!=v){
+            printf("%d edge (%d->%d) = %d\n", nc++, a, b, min);
+            min_cost+=min;
+            parent[v] = u;
+        }
+        cost[a][b] = cost[b][a] = 999;
     }
-    cost[a][b] = cost[b][a] = 999;
-   }
-   printf("\n minimum cost = %d\n", min_cost);
+    printf("\n min cost is : %d", min_cost);
+
     return 0;
 }
