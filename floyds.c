e5b547cf39004864443c3b56a66edc4d51257767
yashodhangurav <guravyashodhan@gmail.com>
2026-06-03 19:14:47 +0530

    practice floyds

--- floyds.c
+++ floyds.c
@@ -1,4 +1,61 @@
-// shortest path problem using floyds algorithm
+// // shortest path problem using floyds algorithm
+
+// #include<stdio.h>
+
+// int min(int a, int b){
+//     if(a<b) return a;
+//     else return b;
+// }
+
+// int floyds(int a[10][10], int n){
+    
+//     int d[10][10];
+
+//     // copying
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             d[i][j] = a[i][j];
+//         }
+//     }
+
+//     for(int k = 0; k<n; k++){
+//         for(int i = 0; i<n; i++){
+//             for(int j = 0; j<n; j++){
+//                 d[i][j] = min(d[i][j], d[i][k]+d[k][j]);
+//             }
+//         }
+//     }
+
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             printf("%d\t", d[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
+
+// int main(){
+//     int a[10][10], n;
+
+//     printf("Enter the number of the vertices: ");
+//     scanf("%d", &n);
+
+//     printf("Enter the adjacency matrix: \n");
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     floyds(a,n);
+
+//     return 0;
+// }
+
+
+
+
+// ------------------------------------PRACTICE-------------------------------------
 
 #include<stdio.h>
 
@@ -7,8 +64,8 @@ int min(int a, int b){
     else return b;
 }
 
-int floyds(int a[10][10], int n){
-    
+
+void floyds(int a[10][10], int n){
     int d[10][10];
 
     for(int i = 0; i<n; i++){
@@ -25,13 +82,15 @@ int floyds(int a[10][10], int n){
         }
     }
 
+    // print
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
-            printf("%d\t", d[i][j]);
+            printf("%d\t", d[j][i]);
         }
         printf("\n");
     }
 }
+
 
 int main(){
     int a[10][10], n;
@@ -40,6 +99,7 @@ int main(){
     scanf("%d", &n);
 
     printf("Enter the adjacency matrix: \n");
+
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             scanf("%d", &a[i][j]);
@@ -47,6 +107,7 @@ int main(){
     }
 
     floyds(a,n);
+
 
     return 0;
 }
