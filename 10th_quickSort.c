a34bd65891469b6aa99b7d6d5e3612e83f1282ae
Yashodhan <yashodhan@Yashodhans-MacBook-Air.local>
2026-07-19 12:33:29 +0530

    practice session

--- 10th_quickSort.c
+++ 10th_quickSort.c
@@ -58,57 +58,184 @@
 
 
 
+// #include<stdio.h>
+
+// int partition(int *arr, int si,int ei){
+//     int i = si+1;
+//     int j = ei;
+
+//     int pivot = arr[si];
+
+//     while(i<=j){
+//         if(pivot>arr[i]){
+//             i++;
+//         }else if(pivot<arr[j]){
+//             j--;
+//         }else{
+//             int temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//         }
+//     }
+//     int temp = arr[si];
+//     arr[si] = arr[j];
+//     arr[j] = temp;
+
+
+//     return j;
+// }
+
+// void quickSort(int *arr, int si, int ei){
+//     if(si>=ei){
+//         return;
+//     }
+
+//     int partitionIdx = partition(arr,si,ei);
+
+//     quickSort(arr,si,partitionIdx-1);
+//     quickSort(arr,partitionIdx+1,ei);
+// }
+
+
+// void printfn(int *arr, int n){
+//     for(int i = 0; i<n; i++){
+//         printf("%d ", arr[i]);
+//     }
+// }
+
+// int main(){
+//     int arr[] = {5,7,1,3,9,4};
+//     int n = sizeof(arr)/sizeof(int);
+
+//     quickSort(arr, 0, n-1);
+//     printfn(arr,n);
+
+//     return 0;
+// }
+
+
+
+// ----------------------------------------practice----------------------------
+
+// #include<stdio.h>
+
+
+// int partition(int *arr, int si,int ei){
+//     int i = si+1;
+//     int j = ei;
+//     int pivot = arr[si];
+
+//     while(i<=j){
+//         if(pivot > arr[i]){
+//             i++;
+//         }else if(pivot < arr[j]){
+//             j--;
+//         }else{
+//             int temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//             i++;
+//             j--;
+//         }
+//     }
+//     int temp = arr[si];
+//     arr[si] = arr[j];
+//     arr[j] = temp;
+
+
+//     return j;
+// }
+
+// void quickSort(int arr[], int si, int ei){
+//     if(si>=ei){
+//         return;
+//     }
+
+//     int partitionIdx = partition(arr,si,ei);
+
+//     quickSort(arr,si,partitionIdx-1);
+//     quickSort(arr, partitionIdx+1, ei);
+// }
+
+
+// void print(int *arr, int n){
+//     for(int i = 0; i<n; i++){
+//         printf("%d ", arr[i]);
+//     }
+// }
+
+// int main(){
+
+//     int arr[] = {3,6,8,3,4,2,1,9};
+//     int n = sizeof(arr)/sizeof(int);
+
+//     quickSort(arr,0,n-1);
+//     print(arr,n);
+
+//     return 0;
+// }
+
+
+
+// ----------------------------------------practice
+
+
 #include<stdio.h>
 
-int partition(int *arr, int si,int ei){
+
+int partition(int arr[], int si, int ei){
     int i = si+1;
     int j = ei;
 
     int pivot = arr[si];
 
+    
     while(i<=j){
-        if(pivot>arr[i]){
+        if(pivot > arr[i]){
             i++;
-        }else if(pivot<arr[j]){
+        }else if(pivot < arr[j]){
             j--;
         }else{
             int temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
+            i++;
+            j--;
         }
     }
     int temp = arr[si];
     arr[si] = arr[j];
     arr[j] = temp;
 
-
     return j;
+
 }
 
 void quickSort(int *arr, int si, int ei){
     if(si>=ei){
         return;
     }
-
-    int partitionIdx = partition(arr,si,ei);
+    int partitionIdx = partition(arr, si,ei);
 
     quickSort(arr,si,partitionIdx-1);
     quickSort(arr,partitionIdx+1,ei);
 }
 
 
-void printfn(int *arr, int n){
+void print(int *arr, int n){
     for(int i = 0; i<n; i++){
-        printf("%d ", arr[i]);
+        printf("%d, ", arr[i]);
     }
 }
 
 int main(){
-    int arr[] = {5,7,1,3,9,4};
+
+    int arr[] = {4,7,3,2,4,1};
     int n = sizeof(arr)/sizeof(int);
 
-    quickSort(arr, 0, n-1);
-    printfn(arr,n);
+    quickSort(arr,0,n-1);
+
+    print(arr,n);
 
     return 0;
 }
