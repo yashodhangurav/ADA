a7434247a60f50b75f5af4967a0acea3c9a444c3
Yashodhan <yashodhan@Yashodhans-MacBook-Air.local>
2026-07-19 19:10:40 +0530

    practice session

--- topologicalSort.c
+++ topologicalSort.c
@@ -56,11 +56,188 @@
 
 
 
-// ---------------------------------practice
+// ---------------------------------practice----------------
+
+
+// #include<stdio.h>
+
+
+// int main(){
+
+//     int n, a[10][10], indegree[10] = {0};
+//     int stack[10], top = -1;
+//     int topo[10], k = 0;
+
+
+//     printf("enter the number of vertice: ");
+//     scanf("%d", &n);
+
+//     printf("adjucency matric: ");
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     for(int j = 0; j<n; j++){
+//         for(int i = 0; i<n; i++){
+//             indegree[j] += a[i][j];
+//         }
+//     }
+
+//     for(int i = 0; i<n; i++){
+//         if(indegree[i] == 0){
+//             stack[++top] = i;
+//         }
+//     }
+
+
+//     while(top != -1){
+
+//         int u = stack[top--];
+//         topo[k++] = u;
+
+//         for(int i = 0; i<n; i++){
+//             if(a[u][i] == 1){
+//                 indegree[i]--;
+
+//                 if(indegree[i] == 0){
+//                     stack[++top] = i;
+//                 }
+//             }
+//         }
+//     }
+
+//     printf("topo sort :");
+//     for(int i = 0; i<k; i++){
+//         printf("%d\t", topo[i]);
+//     }
+//     return 0;
+// }
+
+
+// ---------------------------------practice----------------
+
+
+// #include<stdio.h>
+
+
+// int main(){
+
+//     int n, a[10][10], indegree[10] = {0};
+//     int stack[10], top = -1;
+//     int topo[10], k = 0;
+
+//     printf("Enter the number of the vertices : ");
+//     scanf("%d", &n);
+
+//     printf("Enter the adjucency matrix: ");
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     for(int j = 0; j<n; j++){
+//         for(int i = 0; i<n; i++){
+//             indegree[j] += a[i][j];
+//         }
+//     }
+
+//     for(int i = 0; i<n; i++){
+//         if(indegree[i] == 0){
+//             stack[++top] = i;
+//         }
+//     }
+
+//     while(top != -1){
+//         int u = stack[top--];
+//         topo[k++] = u;
+
+//         for(int i = 0; i<n; i++){
+//             if(a[u][i] == 1){
+//                 indegree[i]--;
+
+//                 if(indegree[i] == 0){
+//                     stack[++top] = i;
+//                 }
+//             }
+//         }
+//     }
+
+//     printf("Topological sort : ");
+//     for(int i = 0; i<n; i++){
+//         printf("%d ", topo[i]);
+//     }
+
+
+//     return 0;
+// }
+
+
+
+// ---------------------------------practice----------------
+
+
+// #include<stdio.h>
+
+// int main(){
+
+//     int n, a[10][10], indegree[10] = {0};
+//     int stack[10], top = -1;
+//     int topo[10], k = 0;
+
+//     printf("Enter the number of the vertices : ");
+//     scanf("%d", &n);
+
+//     printf("Enter the adjucency matrix : ");
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<n; j++){
+//             scanf("%d", &a[i][j]);
+//         }
+//     }
+
+//     for(int j = 0; j<n; j++){
+//         for(int i = 0; i<n; i++){
+//             indegree[j] += a[i][j];
+//         }
+//     }
+//     for(int i = 0; i<n; i++){
+//         if(indegree[i] == 0){
+//             stack[++top] = i;
+//         }
+//     }
+
+//     while(top != -1){
+//         int u = stack[top--];
+//         topo[k++] = u;
+
+//         for(int i = 0; i<n; i++){
+//             if(a[u][i] == 1){
+//                 indegree[i]--;
+
+//                 if(indegree[i] == 0){
+//                     stack[++top] = i;
+//                 }
+//             }
+//         }
+//     }
+
+//     printf("topological sort : ");
+//     for(int i = 0; i<n; i++){
+//         printf("%d ", topo[i]);
+//     }
+
+//     return 0;
+// }
+
+
+
+// ---------------------------------practice----------------
+
 
 
 #include<stdio.h>
-
 
 int main(){
 
@@ -69,10 +246,11 @@ int main(){
     int topo[10], k = 0;
 
 
-    printf("enter the number of vertice: ");
+
+    printf("Enter the number of the vertices : ");
     scanf("%d", &n);
 
-    printf("adjucency matric: ");
+    printf("Enter the adjucency matrix : ");
     for(int i = 0; i<n; i++){
         for(int j = 0; j<n; j++){
             scanf("%d", &a[i][j]);
@@ -80,7 +258,7 @@ int main(){
     }
 
     for(int j = 0; j<n; j++){
-        for(int i = 0; i<n; i++){
+        for(int i = 0; i<n;i++){
             indegree[j] += a[i][j];
         }
     }
@@ -91,9 +269,7 @@ int main(){
         }
     }
 
-
     while(top != -1){
-
         int u = stack[top--];
         topo[k++] = u;
 
@@ -107,10 +283,8 @@ int main(){
             }
         }
     }
-
-    printf("topo sort :");
-    for(int i = 0; i<k; i++){
-        printf("%d\t", topo[i]);
+    printf("topo sort: ");
+    for(int i = 0; i<n; i++){
+        printf("%d ", topo[i]);
     }
-    return 0;
 }
