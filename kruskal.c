b04c328485d219b8f387ce338a13c8ae9903ec9a
Yashodhan <yashodhan@Yashodhans-MacBook-Air.local>
2026-07-19 20:47:29 +0530

    initial commit

--- kruskal.c
+++ kruskal.c
@@ -389,3 +389,67 @@
 // -----------------------------------practice-------------------------------------
 
 
+#include<stdio.h>
+
+
+int nc = 1, min_cost = 0;
+
+int main(){
+
+    int n, min;
+    int a,b,u,v;
+    int cost[10][10], parent[10];
+
+
+    printf("Enter the number of the vertices : ");
+        scanf("%d", &n);
+    
+
+    printf("Enter the cost adjucency matrix : ");
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=n; j++){
+            scanf("%d", &cost[i][j]);
+
+            if(cost[i][j] == 0){
+                cost[i][j] = 999;
+            }
+        }
+    }
+
+    for(int i = 1; i<=n; i++){
+        parent[i] = 0;
+    }
+
+    printf("Edgees of the mini spanning tree : ");
+    while(nc<n){
+        min = 999;
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                if(cost[i][j] < min){
+                    min = cost[i][j];
+                    a = u = i;
+                    b = v = j;
+                }
+            }
+        }
+
+        while(parent[u]){
+            u = parent[u];
+        }
+        while(parent[v]){
+            v = parent[v];
+        }
+
+        if(u!=v){
+            printf(" %d edge (%d -> %d) = %d\n", nc++,a,b,min);
+            min_cost += min;
+            parent[v] = u;
+        }
+        cost[a][b] = cost[b][a] = 999;
+    }
+    printf("The mini cost is : %d", min_cost);
+
+    return 0;
+
+}
