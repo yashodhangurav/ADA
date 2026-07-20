e18d37e13a51d4660d760137f482b287db699218
Yashodhan <yashodhan@Yashodhans-MacBook-Air.local>
2026-07-20 14:25:28 +0530

    practice session

--- 12th_nQueens.c
+++ 12th_nQueens.c
@@ -307,3 +307,80 @@
 
 
 // -----------------------------------------prctice---------------------------
+
+
+
+
+#include<stdio.h>
+
+#define N 5
+
+
+void printBoard(char board[N][N]){
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<N; j++){
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+    printf("-----------------------------\n");
+}
+
+
+
+int isSafe(char board[N][N], int row, int col){
+
+    // verticle check
+    for(int i = 0; i<N; i++){
+        if(board[i][col] == 'Q'){
+           return 0; 
+        }
+        
+    }
+    // upper left check
+    for(int i = row, j = col; i>=0 && j>=0; i--,j--){
+        if(board[i][j] == 'Q'){
+            return 0;
+        }
+    }
+    // upper right 
+    for(int i = row, j = col; i >= 0 && j < N; i--,j++){
+        if(board[i][j] == 'Q'){
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+
+
+void nQueens(char board[N][N], int row){
+    if(row == N){
+        printBoard(board);
+        return;
+    }
+
+    for(int j = 0; j<N; j++){
+        if(isSafe(board, row, j)){
+            board[row][j] = 'Q';
+            nQueens(board, row+1);
+            board[row][j] = '.';
+        }
+    }
+}
+
+int main(){
+
+    char board[N][N];
+
+    for(int i = 0; i<N; i++){
+        for(int j = 0; j<N; j++){
+            board[i][j] = '.';
+        }
+    }
+
+    nQueens(board,0);
+
+    return 0;
+}
